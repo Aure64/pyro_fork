6ef0146cfef6d4b24453c625ecd654b3e0a56828
Dennis <dfalling@gmail.com>
2020-12-15 16:15:05 +0000

    Add prettier linting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,5 +2,5 @@ console.log("Server started");
 
 // dummy timer to keep process alive.  Will replace in future once server is implemented.
 setInterval(() => {
-    console.log("Polling...");
+  console.log("Polling...");
 }, 1000 * 60 * 60);
