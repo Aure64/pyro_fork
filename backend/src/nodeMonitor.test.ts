948620cf4ffad039f38e20cc2a904a829d5be566
Dennis Falling <dfalling@gmail.com>
2021-02-04 15:33:31 -0300

    Separate node monitor data fetching and checking

--- backend/src/nodeMonitor.test.ts
+++ backend/src/nodeMonitor.test.ts
@@ -1,77 +1,93 @@
-import { checkBlockByHash } from "./nodeMonitor";
-import { RpcClient } from "@taquito/rpc";
-import { enableFetchMocks } from "jest-fetch-mock";
+import { checkBlockInfo, BootstrappedStatus } from "./nodeMonitor";
 
-enableFetchMocks();
-
-describe("checkBlockByHash", () => {
-  const rpc = ({} as unknown) as RpcClient;
-
+describe("checkBlockInfo", () => {
   test("returns event when node is behind", async () => {
-    const status = { bootstrapped: true, sync_state: "unsynced" };
-    fetchMock.mockResponse(JSON.stringify(status));
-    const events = await checkBlockByHash({
-      node: "http://somenode",
-      rpc,
-      blockHash: "some_hash",
-      previousStatus: undefined,
+    const bootstrappedStatus: BootstrappedStatus = {
+      bootstrapped: true,
+      sync_state: "unsynced",
+    };
+    const node = "http://somenode";
+    const head = "some_block";
+    const nodeInfo = { head, bootstrappedStatus };
+    const previousNodeInfo = undefined;
+    const events = checkBlockInfo({
+      node,
+      nodeInfo,
+      previousNodeInfo,
     });
-    expect(events).toEqual({
-      events: [
-        {
-          kind: "NODE_BEHIND",
-          node: "http://somenode",
-          type: "PEER",
-          message: "Node is behind",
-        },
-      ],
-      status,
-    });
+    expect(events).toEqual([
+      {
+        kind: "NODE_BEHIND",
+        node: "http://somenode",
+        type: "PEER",
+        message: "Node is behind",
+      },
+    ]);
   });
 
   test("returns empty events when node is synced", async () => {
-    const status = { bootstrapped: true, sync_state: "synced" };
-    fetchMock.mockResponse(JSON.stringify(status));
-    const events = await checkBlockByHash({
-      node: "http://somenode",
-      rpc,
-      blockHash: "some_hash",
-      previousStatus: undefined,
+    const bootstrappedStatus: BootstrappedStatus = {
+      bootstrapped: true,
+      sync_state: "synced",
+    };
+    const node = "http://somenode";
+    const head = "some_block";
+    const nodeInfo = { head, bootstrappedStatus };
+    const previousNodeInfo = undefined;
+    const events = checkBlockInfo({
+      node,
+      nodeInfo,
+      previousNodeInfo,
     });
-    expect(events).toEqual({ events: [], status });
+    expect(events).toEqual([]);
   });
 
   test("returns empty events when node is unsynced, but still bootstrapping", async () => {
-    const status = { bootstrapped: false, sync_state: "unsynced" };
-    fetchMock.mockResponse(JSON.stringify(status));
-    const events = await checkBlockByHash({
-      node: "http://somenode",
-      rpc,
-      blockHash: "some_hash",
-      previousStatus: undefined,
+    const bootstrappedStatus: BootstrappedStatus = {
+      bootstrapped: false,
+      sync_state: "unsynced",
+    };
+    const node = "http://somenode";
+    const head = "some_block";
+    const nodeInfo = { head, bootstrappedStatus };
+    const previousNodeInfo = undefined;
+    const events = checkBlockInfo({
+      node,
+      nodeInfo,
+      previousNodeInfo,
     });
-    expect(events).toEqual({ events: [], status });
+    expect(events).toEqual([]);
   });
 
   test("returns caught up event when node is synced but previously wasn't", async () => {
-    const status = { bootstrapped: true, sync_state: "synced" };
-    fetchMock.mockResponse(JSON.stringify(status));
-    const events = await checkBlockByHash({
-      node: "http://somenode",
-      rpc,
-      blockHash: "some_hash",
-      previousStatus: { bootstrapped: true, sync_state: "unsynced" },
+    const bootstrappedStatus: BootstrappedStatus = {
+      bootstrapped: true,
+      sync_state: "synced",
+    };
+    const node = "http://somenode";
+    const head = "some_block";
+    const nodeInfo = { head, bootstrappedStatus };
+    const oldHead = "some_older_block";
+    const oldBootstrappedStatus: BootstrappedStatus = {
+      bootstrapped: true,
+      sync_state: "unsynced",
+    };
+    const previousNodeInfo = {
+      head: oldHead,
+      bootstrappedStatus: oldBootstrappedStatus,
+    };
+    const events = checkBlockInfo({
+      node,
+      nodeInfo,
+      previousNodeInfo,
     });
-    expect(events).toEqual({
-      events: [
-        {
-          kind: "NODE_CAUGHT_UP",
-          node: "http://somenode",
-          type: "PEER",
-          message: "Node caught up",
-        },
-      ],
-      status,
-    });
+    expect(events).toEqual([
+      {
+        kind: "NODE_CAUGHT_UP",
+        node: "http://somenode",
+        type: "PEER",
+        message: "Node caught up",
+      },
+    ]);
   });
 });
