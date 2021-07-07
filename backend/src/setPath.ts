cf0d2854f900b32b86b0d0a973d3e42dc1fcc6f4
Igor Tkach <igor.tkach@tqtezos.com>
2021-07-07 00:12:53 -0400

    remove ramda dependency

--- backend/src/setPath.ts
+++ backend/src/setPath.ts
@@ -1,14 +1,8 @@
-import * as R from "ramda";
+import { setWith, clone } from "lodash";
 
-/**
- * Creates a new `data` with the `value` set to the nested `path`.  `path` is a UserPref style path
- * delimited by colons.
- */
-const setPath = <T>(path: string, data: T, value: unknown): T => {
+const setPath = (path: string, data: object, value: unknown): object => {
   const objectPath = path.split(":");
-  // create Ramda lens for writing to that path (simplest way to ensure entire path exists)
-  const lensPath = R.lensPath(objectPath);
-  return R.set(lensPath, value, data);
+  return setWith(clone(data), objectPath, value, clone);
 };
 
 export default setPath;
