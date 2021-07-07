91253fbda7c3c9b756f22e89a1ebbd7df7e7718e
Igor Tkach <igor.tkach@tqtezos.com>
2021-07-07 00:26:00 -0400

    fix linter error

--- backend/src/setPath.ts
+++ backend/src/setPath.ts
@@ -1,6 +1,10 @@
 import { setWith, clone } from "lodash";
 
-const setPath = (path: string, data: object, value: unknown): object => {
+const setPath = (
+  path: string,
+  data: Record<string, any>,
+  value: unknown
+): Record<string, any> => {
   const objectPath = path.split(":");
   return setWith(clone(data), objectPath, value, clone);
 };
