8931b5c7c34139ac4f3a1a24e305730514aeb642
Igor Tkach <itkach@gmail.com>
2021-10-12 17:30:26 -0400

    fix another star import

--- backend/src/senders/telegram.ts
+++ backend/src/senders/telegram.ts
@@ -1,4 +1,4 @@
-import * as TelegramBot from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
 import { getLogger } from "loglevel";
 import { Event, Sender } from "../events";
 import format from "../format";
