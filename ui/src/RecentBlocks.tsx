f52c59b24535e532b22905d5757bd1b7e5368032
Igor Tkach <itkach@gmail.com>
2021-10-20 16:16:42 -0400

    tweak relative time format display
    use larger units for longer durations

--- ui/src/RecentBlocks.tsx
+++ ui/src/RecentBlocks.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Box, Text } from '@chakra-ui/react';
-import { ellipsifyMiddle, relativeTimeFormat } from './format';
+import { Box, Text, Tooltip } from '@chakra-ui/react';
+import { ellipsifyMiddle, formatRelativeTime, timestampFormat } from './format';
 
 export default ({
   recentBlocks,
@@ -15,31 +15,31 @@ export default ({
   }>;
 }) => (
   <>
-    {recentBlocks.slice(0, 3).map((block, index) => (
-      <Box
-        key={block.level}
-        d="flex"
-        w="100%"
-        justifyContent="space-between"
-        alignItems="baseline"
-        fontWeight={index ? 'normal' : 'bold'}
-        color={index ? 'gray.600' : 'black'}
-      >
-        <Box>
-          <code>
-            {block.level} {block.priority}
-          </code>{' '}
-          <code>{ellipsifyMiddle(block.hash)}</code>{' '}
+    {recentBlocks.slice(0, 3).map((block, index) => {
+      const blockTimeStamp = new Date(block.timestamp);
+      return (
+        <Box
+          key={block.level}
+          d="flex"
+          w="100%"
+          justifyContent="space-between"
+          alignItems="baseline"
+          fontWeight={index ? 'normal' : 'bold'}
+          color={index ? 'gray.600' : 'black'}
+        >
+          <Box>
+            <code>
+              {block.level} {block.priority}
+            </code>{' '}
+            <code>{ellipsifyMiddle(block.hash)}</code>{' '}
+          </Box>
+          <Tooltip label={timestampFormat.format(blockTimeStamp)}>
+            <Text fontSize="xs" fontFamily="monospace">
+              {formatRelativeTime(blockTimeStamp.getTime())}
+            </Text>
+          </Tooltip>
         </Box>
-        <Text fontSize="xs" fontFamily="monospace">
-          {relativeTimeFormat.format(
-            Math.round(
-              (new Date(block.timestamp).getTime() - Date.now()) / 1000,
-            ),
-            'seconds',
-          )}
-        </Text>
-      </Box>
-    ))}
+      );
+    })}
   </>
 );
--- ui/src/format.ts
+++ ui/src/format.ts
@@ -7,6 +7,34 @@ export const timestampFormat = new Intl.DateTimeFormat([], {
   timeStyle: 'short',
 });
 
+const S_SECOND = 1e3;
+const S_MINUTE = 60 * S_SECOND;
+const S_HOUR = 60 * S_MINUTE;
+const S_DAY = 24 * S_HOUR;
+
+export const formatRelativeTime = (
+  t: number,
+  now: number = Date.now(),
+): string => {
+  const dtMillis = t - now;
+  const dtMillisAbs = Math.abs(dtMillis);
+  let dt, unit;
+  if (dtMillisAbs > 2 * S_DAY) {
+    dt = Math.round(dtMillis / S_DAY);
+    unit = 'day' as Intl.RelativeTimeFormatUnit;
+  } else if (dtMillisAbs > 2 * S_HOUR) {
+    dt = Math.round(dtMillis / S_HOUR);
+    unit = 'hour' as Intl.RelativeTimeFormatUnit;
+  } else if (dtMillisAbs > 2 * S_MINUTE) {
+    dt = Math.round(dtMillis / S_MINUTE);
+    unit = 'minute' as Intl.RelativeTimeFormatUnit;
+  } else {
+    dt = Math.round(dtMillis / S_SECOND);
+    unit = 'second' as Intl.RelativeTimeFormatUnit;
+  }
+  return relativeTimeFormat.format(dt, unit);
+};
+
 export const takeStart = (str: string | undefined | null, length = 5) => {
   return str && `${str.substr(0, length)}`;
 };
