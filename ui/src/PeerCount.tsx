255f7871a8e47bad3da1da5aad03fa4240cbaa70
Igor Tkach <itkach@gmail.com>
2021-10-20 00:10:38 -0400

    fix peer count display when count is zero

--- ui/src/PeerCount.tsx
+++ ui/src/PeerCount.tsx
@@ -1,13 +1,16 @@
 import { Stat, StatHelpText, StatNumber } from '@chakra-ui/react';
 import React from 'react';
 
-export default ({ peerCount }: { peerCount: number | null | undefined }) => (
-  <Stat paddingLeft="5px" flexGrow={0} textAlign="center">
-    <StatNumber
-      color={peerCount ? (peerCount < 4 ? 'red' : 'black') : 'gray.400'}
-    >
-      {peerCount || '?'}
-    </StatNumber>
-    <StatHelpText marginBottom={0}>peers</StatHelpText>
-  </Stat>
-);
+export default ({ peerCount }: { peerCount: number | null | undefined }) => {
+  const hasCount = typeof peerCount === 'number';
+  return (
+    <Stat paddingLeft="5px" flexGrow={0} textAlign="center">
+      <StatNumber
+        color={hasCount ? (peerCount < 4 ? 'red' : 'black') : 'gray.400'}
+      >
+        {hasCount ? peerCount : '?'}
+      </StatNumber>
+      <StatHelpText marginBottom={0}>peers</StatHelpText>
+    </Stat>
+  );
+};
