ab691cc2c148e0d0061573c320be00d08583d666
Igor Tkach <itkach@gmail.com>
2021-10-29 21:23:35 -0400

    remove unused imports

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
-import { VStack, Divider, Heading } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 import React from 'react';
-import Nodes from './Nodes';
 import Bakers from './Bakers';
+import Nodes from './Nodes';
 import useInterval from './use-interval';
 
 interface AppProps {}
